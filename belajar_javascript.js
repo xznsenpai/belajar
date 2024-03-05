8970b7f07bc929dae6d5ce353bcdd87e8a8ddc54
xznsenpai <zainudinafgan@gmail.com>
2024-03-05 10:32:52 +0800

    up

--- belajar_javascript.js
+++ belajar_javascript.js
@@ -5,7 +5,41 @@ karakter atau substring yang di cari dalam string, jika
 tidak di temukan, di kembalikan -1.
 */
 const string = "Nama Aku Zeiyy, Aku hoby membaca, dengar music, dan main voly";
-const index = string.indexOf();
-console.log(index); //Output 
+const index = string.indexOf("Zeiyy");
+console.log(index); //Output: 9
 
+/*
+toLowerCase()
+dapat mengambalikan string dengan semua karakternya
+menjadi huruf kecil.
+*/
+const string = "Zeiyy Belajar JavaScript";
+const index = string.toLowerCase();
+console.log(index); //Output: zeiyy belajar javascript
 
+/*
+includes()
+digunakan untuk memeriksa apakah sebuah string mengandung
+string atau karakter yang di tentukan.
+*/
+const string = "Belajar Javascript Zeiyy Sangat suka";
+const index = string.includes("Zeiyy");
+console.log(index); //Output: true
+
+/*
+endsWith()
+fungsi memeriksa apakah sebuah string di akhiri dengan string atau
+dengan karakter di tentukan.
+*/
+const string = "Belajar Javascript Zeiyy Sangat suka";
+const index = string.endsWith("suka");
+console.log(index); //Output: true
+
+/*
+repeat()
+ini mengambalikan string baru dengan jumlah 
+salinan tertentu dan string yang ada.
+*/
+const string = "Oke_Gas ";
+const index = string.repeat(3);
+console.log(index); //Output: Oke_Gas Oke_Gas Oke_Gas

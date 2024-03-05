ff0a496db3cfe2a2a50ec967328cc7b858b76445
Zeiyy <81281058+xznsenpai@users.noreply.github.com>
2024-03-05 09:49:56 +0800

    update belajar_javascript.js

---
+++ belajar_javascript.js
@@ -0,0 +1,11 @@
+/*
+indexOf()
+mencari dan (jika di temukan) mengambalikan nomor indexs
+karakter atau substring yang di cari dalam string, jika
+tidak di temukan, di kembalikan -1.
+*/
+const string = "Nama Aku Zeiyy, Aku hoby membaca, dengar music, dan main voly";
+const index = string.indexOf();
+console.log(index); //Output 
+
+
